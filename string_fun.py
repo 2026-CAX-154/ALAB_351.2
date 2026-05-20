3da393ac8f3a6da4c2723a5f176d871b9edbfeff
EKim-NY <erenkim@gmail.com>
2026-05-19 21:11:01 -0400

    Add description about Python script
    This Python script accepts a word as user input and performs the following tasks: 
    - Print out the word length. 
    - Print out the world in all uppercase.
    - Print out the word three times (on the same line).
    - Print out the word three times (on separate lines).

---
+++ string_fun.py
@@ -0,0 +1,10 @@
+# ALAB 351.2 
+# Part 2: User Interaction and Input
+# Author: Eren Kim
+
+# This script accepts a word as user input to print out: 
+# - the word length
+# - the word in all uppercase
+# - the word repeated 3 times (on the same line)
+# - the word repeated 3 times (on separate lines)
+
