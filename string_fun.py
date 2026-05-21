c69261d4cf86e16a2a8e7b9c7c7f7fb18dd39bff
EKim-NY <erenkim@gmail.com>
2026-05-20 21:48:12 -0400

    Add completed script

--- string_fun.py
+++ string_fun.py
@@ -8,3 +8,11 @@
 # - the word repeated 3 times (on the same line)
 # - the word repeated 3 times (on separate lines)
 
+word = str(input("Enter a word to play with: ")) # Prompt user for a word
+print("The word length is", len(word), ".") # Print word length
+print("The word in all uppercase is", word.upper()) # Print word in uppercase
+print("The word printed out 3 times in a row is",word * 3) # Print word 3 times in a row
+print("The word printed out 3 times on separate lines is:") # Print word 3 times on separate lines
+print(word, word, word, sep="\n")
+
+# End of Python script 
